9cd2280f5df02bc9c4f5216f5c571d92c6c23503
RealVinnykk <85770189+RealVinnykk@users.noreply.github.com>
2023-11-22 20:57:18 -0300

    Algumas aulas assistidas e modificação em documentos js

---
+++ JavaScript/cursoGameDev-DIO/aula.js
@@ -0,0 +1,8 @@
+gasPrice = 3.50
+
+kilometersPerGas = 5
+liters = 3
+totalGas = gasPrice * liters 
+tripDistance = 15
+
+console.log("a viagem tem " + tripDistance + "KM de distancia, por isso teremos que usar pelo meno 3 litros de gasolina, que dará o total de " + totalGas + " Reais")
