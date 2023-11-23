fed7b0cae6ed5d4e868e44f16c4cd300f9ee8093
RealVinnykk <85770189+RealVinnykk@users.noreply.github.com>
2023-11-23 13:47:46 -0300

    added two ways of aloneTesting

--- aloneTesting/fuzzbuzz.js
+++ aloneTesting/fuzzbuzz.js
@@ -5,20 +5,39 @@ loop = 100
 while (i < loop) {
     i++
 
+// * If version
+
     console.log(i)
 
     if (i % 3 == 0) {
-        console.log("Buzz")
+        console.log("\nBuzz")
     }
 
     if (i % 5 == 0) {
-        console.log("Fuzz")
+        console.log("\nFuzz")
     }
 
     if (i % 3 && 5 == 0) {
-        console.log("FuzzBuzz")
+        console.log("\nFuzzBuzz")
     }
 
 }
 
+// ^ Switch version 
+
+// switch (i < loop) {
+//     case i % 3 == 0:
+//         console.log(i + " Buzz")
+//         break;
+
+//     case i % 5 == 0:
+//         console.log(i + " Fuzz")
+//         break;
+
+//     case i % 7 == 0:
+//         console.log(i + " FuzzBuzz")
+//         break;
+//     default:
+//         break;
+// }
 
