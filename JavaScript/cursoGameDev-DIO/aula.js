a1d5e552dc109a013064bdc14919adab8e722c53
RealVinnykk <85770189+RealVinnykk@users.noreply.github.com>
2023-11-22 21:20:03 -0300

    finishing class and challenge, with README updates

--- JavaScript/cursoGameDev-DIO/aula.js
+++ JavaScript/cursoGameDev-DIO/aula.js
@@ -1,8 +1,21 @@
-gasPrice = 3.50
+// COmments Details and guide for colors
+//  ! Red
+//  ? Blue
+//  * Green
+//  ^ Yellow
+//  & Pink
+//  ~ Purple
+//  todo Mustard
+// (//) Greys
+// double "//" for a cut
 
-kilometersPerGas = 5
-liters = 3
-totalGas = gasPrice * liters 
-tripDistance = 15
 
-console.log("a viagem tem " + tripDistance + "KM de distancia, por isso teremos que usar pelo meno 3 litros de gasolina, que dará o total de " + totalGas + " Reais")
+// ? Trip details, such as distance and prices
+
+const gasPrice = 3.5
+const kilometersPerLiter = 5
+let liters = 3
+const totalGas = gasPrice * liters 
+let tripDistance = 15
+
+console.log("a viagem tem " + tripDistance + "KM de distancia, e sabendo que nosso carro faz " + kilometersPerLiter +  " kilometros por litro, teremos que usar pelo meno 3 litros de gasolina, e sabendo que a gasolina esta no valor de " + gasPrice + " o total será de " + totalGas + " Reais")
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # MyCodes
-All my recent codes
+All my recent codes, owner Vinny
+
+
+## This is a repository for my codes
+
+
+| -------------------------------- |
+
+ There's going to be things as 
+ 
+ - Sketches for programs
+ - Messed up codes
+ - Failures 
+ - Good codes 
+ - Annotations
+ - Some fun stuff
+
+| -------------------------------- |
