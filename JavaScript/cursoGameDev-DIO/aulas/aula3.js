bc89b271dd234de7d11a9cf194e7c1507ebeddf0
RealVinnykk <85770189+RealVinnykk@users.noreply.github.com>
2023-11-24 17:18:53 -0300

    New Things

--- JavaScript/cursoGameDev-DIO/aulas/aula3.js
+++ JavaScript/cursoGameDev-DIO/aulas/aula3.js
@@ -1,5 +1,14 @@
 // objetos 
 
-for (let i = 0; i < 100; i++) {
-    
+for (let i = 0; i <= 100; i++) {
+
+    if (i % 3 === 0) {
+        console.log("Fizz")
+    }
+    if (i % 5 === 0) {
+        console.log("Buzz")
+    }
+    if (i % 7 === 0) {
+        console.log("FizzBuzz")
+    }else{console.log(i)}
 }
---
+++ aloneTesting/arrays.js
@@ -0,0 +1,12 @@
+
+function pokemon(nome, level) {
+    nome,
+    level,
+    ATP = level * 1.2,
+    HP = level * 1.3,
+    console.log("Name: " + nome +  "\nLevel: "+ level +"\nHealth Points: "+ HP + "\nAttack Points: " + ATP)
+}
+
+let ippo = (pokemon("Ippo", 30))
+
+console.log(ippo)
