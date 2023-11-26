d10055a993d75dd9497f6ca106d68564bdad31d2
RealVinnykk <you@example.com>
2023-11-26 13:29:15 -0300

    new things that were posted in instagram

--- aloneTesting/semnome/teste.js
+++ aloneTesting/semnome/teste.js
@@ -1,4 +1,4 @@
 const { data, logar } = require('./teste2')
 // ? Dados para serem entrados = Numero de pacotes, Litros de gasolina e quilometros dirigidos
 
-logar(data(60, 35, 100))
+logar(data(65, 75, 100))
