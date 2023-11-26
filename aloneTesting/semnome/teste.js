93f04faa8255ac3edf08bd7b07a013b4d940f70e
RealVinnykk <you@example.com>
2023-11-26 19:47:04 -0300

    New thing using import

--- aloneTesting/semnome/teste.js
+++ aloneTesting/semnome/teste.js
@@ -1,4 +1,15 @@
-const { data, logar } = require('./teste2')
+const { data, logar, dadosMotorista } = require('./teste2')
 // ? Dados para serem entrados = Numero de pacotes, Litros de gasolina e quilometros dirigidos
 
-logar(data(65, 75, 100))
+
+data(65, 75, 100)
+
+console.log('Hoje sua rota teve o lucro de ' + avarageValue + '$ Por pacote, e teve a media de ' + Math.round(kmPerLitter) + ' de quilometros por litro')
+
+var kmMotorista = [];
+kmMotorista.push()
+
+// var pacotesMotorista = [];
+// pacotesMotorista.push(numberPackadges)
+
+console.log(dadosMotorista)
--- aloneTesting/semnome/teste2.js
+++ aloneTesting/semnome/teste2.js
@@ -1,13 +1,26 @@
-function data(numberPackadges, gasLitters, kmDrived) {
+function data(kmDrived, gasLitters, numberPackadges) {
     numberPackadges;
     gasLitters;
     kmDrived;
+    kmDrived = kmDrived
     kmPerLitter = kmDrived / gasLitters;
     avarageValue = numberPackadges / 2.5
-    console.log('Hoje sua rota teve o lucro de ' + avarageValue + '$ Por pacote, e teve a media de ' + Math.floor(kmPerLitter) + ' de quilometros por litro')
+    // console.log('Hoje sua rota teve o lucro de ' + avarageValue + '$ Por pacote, e teve a media de ' + Math.floor(kmPerLitter) + ' de quilometros por litro')
+    return kmPerLitter, avarageValue, kmDrived, numberPackadges ;
 }
 
 function logar(texto){
     console.log(texto)
 }
-module.exports = { data, logar }
+
+
+
+dadosMotorista = []
+dadosMotorista.push(data(15, 450, 520))
+dadosMotorista.push(data(90 , 450, 430))
+dadosMotorista.push(data(42, 450, 90))
+dadosMotorista.push(data(56, 450, 150))
+dadosMotorista.push(data(35, 450, 120))
+
+
+module.exports = { data, logar, dadosMotorista }
