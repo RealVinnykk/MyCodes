97e59a5fae9613d4a302d1b932a9c4cbb2f6f0f5
RealVinnykk <85770189+RealVinnykk@users.noreply.github.com>
2023-11-27 21:34:32 -0300

    Added a Js and Py files that make the same thing, but in the different type

---
+++ aloneTesting/semnome/tentativa1.js
@@ -0,0 +1,20 @@
+
+
+function calcularDistancia(kmDriven, Litters, Distance) {
+    kmDriven = kmDriven;
+    Litters = Litters;
+    Distance = Distance;
+
+    kmPerLitter = Litters / kmDriven
+
+    return console.log('The car has made a estimated amount of ' + kmPerLitter + ' kilometers per liter of gasoline!')
+} 
+
+
+calcularDistancia(100, 90, 50)
+
+//? Concluido JavaScript em mais ou menos 2.3 minutos
+
+
+
+
--- aloneTesting/semnome/tentativa1.py
+++ aloneTesting/semnome/tentativa1.py
@@ -1,4 +1,11 @@
-num1; 
-num2;
+kmDriven = 150
+litters = 75
+distance = int
 
-input()
+kmPerLitter = (litters / kmDriven)
+
+
+print(kmPerLitter)
+
+
+# ? Python concluido em mais ou menos 3 minutos por conta de erro de logica kk
